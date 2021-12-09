6bf3aa64894afe30c2a350828391fbb4a82bf753
Marcin Grzebyk <marcin15g@gmail.com>
2021-12-09 13:33:08 +0100

    Creating new dots

--- particles.js
+++ particles.js
@@ -16,6 +16,17 @@ window.addEventListener('mousemove', (e) => {
     mouse.y = e.y;
 })
 
+window.addEventListener('click', (e) => {
+    const size = (Math.random() * 10 + 5)
+    const x = e.x * 2;
+    const y = e.y * 2 + window.scrollY;
+    const dirX = (Math.random() * 5) - 3;
+    const dirY = (Math.random() * 5) - 3;
+    const color = 'rgb(255, 153, 0)';
+
+    particlesArray.push(new Particle(x, y, dirX, dirY, size, color, true))
+})
+
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth * 2;
     canvas.height = window.innerHeight * 4;
@@ -25,20 +36,21 @@ window.addEventListener('resize', () => {
 })
 
 class Particle {
-    constructor(x, y, dirX, dirY, size, color) {
+    constructor(x, y, dirX, dirY, size, color, custom = false) {
         this.x = x;
         this.y = y;
         this.dirX = dirX;
         this.dirY = dirY;
         this.size = size;
         this.color = color;
+        this.custom = custom;
     }
 
     //Draw individual particle
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        ctx.fillStyle = "#fff";
+        ctx.fillStyle = this.color;
         ctx.fill();
     }
 
@@ -55,7 +67,7 @@ class Particle {
 }
 
 const init = () => {
-    const numOfParticles = 100//(canvas.width * canvas.height) / 15000;
+    const numOfParticles = 150//(canvas.width * canvas.height) / 15000;
     for(let i = 0; i < numOfParticles; i++) {
         const size = (Math.random() * 5 + 1)
         const x = (Math.random() * ((canvas.width - size * 2) - (size * 2)) + size * 2);
@@ -77,20 +89,37 @@ const animate = () => {
 
 const connect = () => {
     for(let a = 0; a < particlesArray.length; a++) {
+
+        //Connect dots to each other
         for(let b = a; b < particlesArray.length; b++) {
             const distanceX = (particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x);
             const distanceY = (particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y);
             const distance = distanceX + distanceY;
             if(distance < ((canvas.width/7) * (canvas.height/7))) {
                 const opacity = 1 - distance/((canvas.width/7) * (canvas.height/7));
-                ctx.strokeStyle = `rgba(255,255,255,${opacity})`;
+                ctx.strokeStyle = particlesArray[a].custom === true && particlesArray[b].custom === true ? `rgba(255,153,0,${opacity})` : `rgba(255,255,255,${opacity})`;
                 ctx.lineWidth = 1;
                 ctx.beginPath();
-                ctx.moveTo(particlesArray[a].x + 0.5, particlesArray[a].y + 0.5);
-                ctx.lineTo(particlesArray[b].x + 0.5, particlesArray[b].y + 0.5);
+                ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
+                ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
                 ctx.stroke();
             }
         }
+
+        //Connect dots to mouse
+        const distanceX = (particlesArray[a].x - mouse.x * 2) * (particlesArray[a].x - mouse.x * 2);
+        const distanceY = (particlesArray[a].y - mouse.y * 2 - (window.scrollY)) * (particlesArray[a].y - mouse.y * 2 - (window.scrollY));
+        const distance = distanceX + distanceY;
+
+        if(distance < ((canvas.width/7) * (canvas.height/7))) {
+            const opacity = 1 - distance/((canvas.width/7) * (canvas.height/7));
+            ctx.strokeStyle = `rgba(255,255,255,${opacity})`;
+            ctx.lineWidth = 3;
+            ctx.beginPath();
+            ctx.moveTo(particlesArray[a].x , particlesArray[a].y);
+            ctx.lineTo(mouse.x * 2, mouse.y * 2 + (window.scrollY));
+            ctx.stroke();
+        }       
     }
 }
 
